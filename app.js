3d1e12402eff7b3eb6a3820b42c5e262370c64bd
codernosunday <tin369cv@gmail.com>
2024-12-11 20:58:54 +0700

    new update 11-12-2024

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ app.use(cors());
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
+app.use(bodyParser.urlencoded({ extended: false, limit: '1mb' }));
+app.use(bodyParser.json({ limit: '1mb' }));
 require('./routes.js')(app);
 
 module.exports = app;
